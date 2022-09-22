4505f47aaff364278bf3e5236703688366a37a67
Vinícius Queiroz Timóteo <vinicius.queiroz.timoteo@gmail.com>
2022-09-22 11:06:39 -0300

    V 1.01 22/09 casa

--- nucleo_regional_educacao/lib/components/avisos_nre_component.dart
+++ nucleo_regional_educacao/lib/components/avisos_nre_component.dart
@@ -7,56 +7,70 @@ class AvisosNreComponent extends StatelessWidget {
   final Icon? importedIcon;
 
   const AvisosNreComponent(
-      {this.onTapped, this.label, this.icon, this.importedIcon})
-      ;
+      {this.onTapped, this.label, this.icon, this.importedIcon});
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(10),
-        onTap: () => onTapped!(),
-        splashColor: Colors.green[300],
-        highlightColor: Colors.blue[300],
-        child: new Container(
-          height: MediaQuery.of(context).size.height * 0.185,
-          width: MediaQuery.of(context).size.width> 600 ?  MediaQuery.of(context).size.width * 0.18 : MediaQuery.of(context).size.width * 0.25,
-          decoration: BoxDecoration(
-            color: Colors.transparent,
-            shape: BoxShape.rectangle,
-          ),
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.zero,
-              child: Align(
-                alignment: Alignment.center,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.zero,
-                        child: Icon(
-                          icon,
-                          color: Colors.black,
-                          size: 40,
-                        ),
-                      ),
-                      Center(
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top:8.0),
-                            child: Text(
-                              label!,
-                              style: TextStyle(
-                                  color: Colors.black, fontSize: MediaQuery.of(context).size.height*0.020),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 0),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () => onTapped!(),
+          splashColor: Colors.green[300],
+          highlightColor: Colors.blue[300],
+          // ignore: unnecessary_new
+          child: Material(
+            borderRadius: BorderRadiusGeometry.lerp(
+                BorderRadius.circular(10), BorderRadius.circular(10), 5),
+            elevation: 10,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.185,
+              width: MediaQuery.of(context).size.width > 600
+                  ? MediaQuery.of(context).size.width * 0.10
+                  : MediaQuery.of(context).size.width * 0.25,
+              // ignore: prefer_const_constructors
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.rectangle,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.zero,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.zero,
+                            child: Icon(
+                              icon,
+                              color: Colors.black,
+                              size: 40,
                             ),
                           ),
-                        ),
+                          Center(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: Text(
+                                  label!,
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize:
+                                          MediaQuery.of(context).size.height *
+                                              0.020),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
--- nucleo_regional_educacao/lib/pages/municipios_pages/apucarana.dart
+++ nucleo_regional_educacao/lib/pages/municipios_pages/apucarana.dart
@@ -53,79 +53,97 @@ class Apucarana extends StatelessWidget {
                     ),
                     Padding(
                       padding: const EdgeInsets.fromLTRB(8.0, 30, 8, 0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadiusGeometry.lerp(
-                                BorderRadius.circular(10),
-                                BorderRadius.circular(10),
-                                5),
-                            border: Border.all(color: Colors.black)),
-                        width: MediaQuery.of(context).size.width > 600
-                            ? MediaQuery.of(context).size.width * 0.7
-                            : MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height / 6.0,
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                'Núcleo Regional de Educação de Apucarana',
-                                style: TextStyle(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height *
-                                            0.02),
+                      child: Material(
+                        borderRadius: BorderRadiusGeometry.lerp(
+                            BorderRadius.circular(10),
+                            BorderRadius.circular(10),
+                            5),
+                        elevation: 10,
+                        child: Container(
+                          decoration: BoxDecoration(),
+                          width: MediaQuery.of(context).size.width > 600
+                              ? MediaQuery.of(context).size.width * 0.5
+                              : MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height / 6.0,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Núcleo Regional de Educação de Apucarana',
+                                  style: TextStyle(
+                                      fontSize:
+                                          MediaQuery.of(context).size.height *
+                                              0.02),
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(4, 8, 10, 0),
-                              child: Row(
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(4, 8, 10, 0),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      'Rua Lapa, 250 - Centro - CEP 86.800-310',
+                                      style: TextStyle(
+                                        fontSize:
+                                            MediaQuery.of(context).size.width >
+                                                    600
+                                                ? MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.018
+                                                : MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.015,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(4, 8, 0, 0),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      'Apucarana - PR | Fone: 43 3420-1600 - Fax: 43 3420-1664',
+                                      style: TextStyle(
+                                        fontSize:
+                                            MediaQuery.of(context).size.width >
+                                                    600
+                                                ? MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.018
+                                                : MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.015,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Text(
-                                    'Rua Lapa, 250 - Centro - CEP 86.800-310',
-                                    style: TextStyle(
-                                        fontSize: MediaQuery.of(context)
-                                                .size
-                                                .height *
-                                            0.015),
+                                  Icon(
+                                    Icons.place,
+                                    color: Colors.red,
+                                    size: 20.0,
+                                    semanticLabel:
+                                        'Shows Apucarana Localization',
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      launchUrl(Uri.parse(
+                                          "https://www.google.com.br/maps/place/R.+Lapa,+250+-+Centro,+Apucarana+-+PR,+86800-310/@-23.5545826,-51.4770048,15z/data=!4m13!1m7!3m6!1s0x94ec9979787489cd:0x2207f18044757f11!2sR.+Lapa,+250+-+Centro,+Apucarana+-+PR,+86800-310!3b1!8m2!3d-23.5545826!4d-51.4682501!3m4!1s0x94ec9979787489cd:0x2207f18044757f11!8m2!3d-23.5545826!4d-51.4682501"));
+                                    },
+                                    child: Text('Localização'),
                                   ),
                                 ],
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(4, 8, 0, 0),
-                              child: Row(
-                                children: [
-                                  Text(
-                                    'Apucarana - PR | Fone: 43 3420-1600 - Fax: 43 3420-1664',
-                                    style: TextStyle(
-                                        fontSize: MediaQuery.of(context)
-                                                .size
-                                                .height *
-                                            0.015),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.place,
-                                  color: Colors.red,
-                                  size: 20.0,
-                                  semanticLabel:
-                                      'Shows Apucarana Localization',
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    launchUrl(Uri.parse(
-                                        "https://www.google.com.br/maps/place/R.+Lapa,+250+-+Centro,+Apucarana+-+PR,+86800-310/@-23.5545826,-51.4770048,15z/data=!4m13!1m7!3m6!1s0x94ec9979787489cd:0x2207f18044757f11!2sR.+Lapa,+250+-+Centro,+Apucarana+-+PR,+86800-310!3b1!8m2!3d-23.5545826!4d-51.4682501!3m4!1s0x94ec9979787489cd:0x2207f18044757f11!8m2!3d-23.5545826!4d-51.4682501"));
-                                  },
-                                  child: Text('Localização'),
-                                ),
-                              ],
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -135,36 +153,36 @@ class Apucarana extends StatelessWidget {
                   children: [
                     Padding(
                       padding: EdgeInsets.fromLTRB(8, 24, 8, 0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadiusGeometry.lerp(
-                                BorderRadius.circular(10),
-                                BorderRadius.circular(10),
-                                5),
-                            border: Border.all(color: Colors.black)),
-                        width: MediaQuery.of(context).size.width > 600
-                            ? MediaQuery.of(context).size.width * 0.7
-                            : MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height / 9.0,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              AvisosNreComponent(
-                                  label: "Institucional",
-                                  onTapped: () => {print("Institucional")},
-                                  icon: Icons.account_balance),
-                              AvisosNreComponent(
-                                  label: "Avisos",
-                                  onTapped: () => {
-                                        Navigator.pushNamed(context,
-                                            '/apucarana/apucarana_avisos')
-                                      },
-                                  icon: Icons.my_library_books),
-                              AvisosNreComponent(
-                                  label: "Noticias",
-                                  onTapped: () => {print("noticias")},
-                                  icon: Icons.newspaper),
-                            ]),
+                      child: Material(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(40.0),
+                          topRight: Radius.circular(40.0),
+                        ),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width > 600
+                              ? MediaQuery.of(context).size.width * 0.7
+                              : MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height / 9.0,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                AvisosNreComponent(
+                                    label: "Institucional",
+                                    onTapped: () => {print("Institucional")},
+                                    icon: Icons.account_balance),
+                                AvisosNreComponent(
+                                    label: "Avisos",
+                                    onTapped: () => {
+                                          Navigator.pushNamed(context,
+                                              '/apucarana/apucarana_avisos')
+                                        },
+                                    icon: Icons.my_library_books),
+                                AvisosNreComponent(
+                                    label: "Noticias",
+                                    onTapped: () => {print("noticias")},
+                                    icon: Icons.newspaper),
+                              ]),
+                        ),
                       ),
                     ),
                   ],
@@ -173,55 +191,75 @@ class Apucarana extends StatelessWidget {
                   children: [
                     Padding(
                       padding: const EdgeInsets.fromLTRB(10.0, 12, 8, 0),
-                      child: Container(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Text("INSTITUCIONAL"),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: Text(
-                                        "Este Núcleo Regional de Educação atende\n16 (dezesseis) municípios:"),
-                                  )
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 12.0),
-                                    child: Text(
-                                        "Apucarana, Arapongas, Bom Sucesso, Borrazópolis,\nCalifórnia, Cambira, Cruzmaltina, Faxinal,\nJandaia do Sul, Kaloré, Marilândia do Sul, Marumbi,\nMauá da Serra, Novo Itacolomi, Rio Bom e Sabáudia."),
+                      child: Material(
+                        elevation: 5,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(40.0),
+                          topRight: Radius.circular(40.0),
+                        ),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width > 600
+                              ? MediaQuery.of(context).size.width * 0.5
+                              : MediaQuery.of(context).size.width,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 24.0),
+                                  child: Row(
+                                    children: [
+                                      Text("INSTITUCIONAL"),
+                                    ],
                                   ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(4.0,20,4,0),
-                                child: Center(
-                                  child: Container(
-                                    height: MediaQuery.of(context).size.height / 4,
-                                    width: MediaQuery.of(context).size.width,
-                                    decoration: const BoxDecoration(
-                                      image: DecorationImage(
-                                        fit: BoxFit.contain,
-                                        image: AssetImage(
-                                            'assets/images/apucarana_fachada.jpg'),
+                                ),
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 8.0),
+                                      child: Text(
+                                          "Este Núcleo Regional de Educação atende\n16 (dezesseis) municípios:"),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 12.0),
+                                      child: Text(
+                                          "Apucarana, Arapongas, Bom Sucesso, Borrazópolis,\nCalifórnia, Cambira, Cruzmaltina, Faxinal,\nJandaia do Sul, Kaloré, Marilândia do Sul, Marumbi,\nMauá da Serra, Novo Itacolomi, Rio Bom e Sabáudia."),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(4.0, 20, 4, 0),
+                                  child: Center(
+                                    child: Container(
+                                      height:
+                                          MediaQuery.of(context).size.height /
+                                              4,
+                                      width: MediaQuery.of(context).size.width,
+                                      decoration: const BoxDecoration(
+                                        image: DecorationImage(
+                                          fit: BoxFit.contain,
+                                          image: AssetImage(
+                                              'assets/images/apucarana_fachada.jpg'),
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top:20.0),
-                                child: Container(color: Colors.grey[300],child: Text("Horário de Funcionamento: segunda a sexta das 8h às 12h e das 13h às 18h"),),
-                              )
-                            ]),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 20.0),
+                                  child: Container(
+                                    color: Colors.grey[300],
+                                    child: Text(
+                                        "Horário de Funcionamento: segunda a sexta das 8h às 12h e das 13h às 18h"),
+                                  ),
+                                )
+                              ]),
+                        ),
                       ),
                     )
                   ],
